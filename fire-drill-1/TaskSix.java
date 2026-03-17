e04035cddce18924ed1f5ce230a541cc0edb3664
devv-leo <lekan.code@gmail.com>
2026-03-17 14:25:33 +0100

    drill 2, task one

--- fire-drill-1/TaskSix.java
+++ fire-drill-1/TaskSix.java
@@ -2,7 +2,7 @@ public class TaskSix {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             if (i % 4 == 0) {
-                int result = i; // target the number (4 or 8)
+                int result = i;
                 for (int j = 0; j < 5; j++) {
                     System.out.print(result + " ");
                     result = result * i;
---
+++ fire-drill-2/TaskOne.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter 10 scores.");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Score " + (i + 1) + ": ");
+            int score = scanner.nextInt();
+            sum += score;
+        }
+
+        System.out.println("Sum: " + sum);
+        
+    }
+}
