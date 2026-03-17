e81266a9b3510c432b5fc3697892b39fd84e9cdf
oolaoye <lekan0olaoye@gmail.com>
2026-03-17 07:51:56 +0100

    implemented square statments

--- ComparingIntegers.java
+++ ComparingIntegers.java
@@ -14,6 +14,8 @@ public class ComparingIntegers {
         if (number == 100) System.out.printf("%d is equal to 100%n", number);
         if (number < 100) System.out.printf("%d is less than 100%n", number);
 
-
+        if (square > 100) System.out.printf("%d squared (%d) is greater than 100%n", number, square);
+        if (square == 100) System.out.printf("%d squared (%d) is equal to 100%n", number, square);
+        if (square < 100) System.out.printf("%d squared (%d) is less than 100%n", number, square);
     }
 }
