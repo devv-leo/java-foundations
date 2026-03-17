b92d4e336b0abd3e7d732f52e8f238fffcaa1851
oolaoye <lekan0olaoye@gmail.com>
2026-03-17 07:50:12 +0100

    renamed file

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int b = input.nextInt();
+
+        int squareA = a * a;
+        int squareB = b * b;
+        int sum = squareA + squareB;
+        int difference = squareA - squareB;
+
+        System.out.printf("Square of %d is %d\n", a, squareA);
+        System.out.printf("Square of %d is %d\n", b, squareB);
+        System.out.printf("Sum of the squares is %d\n", sum);
+        System.out.printf("Difference of the squares = %d\n", difference);
+
+    }
+}
