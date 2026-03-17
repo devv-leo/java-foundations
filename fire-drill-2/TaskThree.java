3fe76518b48c0bdf680a3011795841fe68543fb3
devv-leo <lekan.code@gmail.com>
2026-03-17 14:39:37 +0100

    drill 2; task 3

---
+++ fire-drill-2/TaskThree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter 10 scores.");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Score " + (i + 1) + ": ");
+            int score = scanner.nextInt();
+            sum += score;
+        }
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + sum/10);
+        
+    }
+}
