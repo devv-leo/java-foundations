e2dc9f6f1e296204b71ffeb79671ce98cb1da0b8
Lekan Olaoye <lekan0olaoye@gmail.com>
2026-03-17 07:56:06 +0100

    compiled ComparingIntegers

--- ComparingIntegers.java
+++ ComparingIntegers.java
@@ -10,12 +10,12 @@ public class ComparingIntegers {
 
         int square = number * number;
 
-        if (number > 100) System.out.printf("%d is greater than 100%n", number);
-        if (number == 100) System.out.printf("%d is equal to 100%n", number);
-        if (number < 100) System.out.printf("%d is less than 100%n", number);
+        if (number > 100) System.out.printf("%d is greater than 100\n", number);
+        if (number == 100) System.out.printf("%d is equal to 100\n", number);
+        if (number < 100) System.out.printf("%d is less than 100\n", number);
 
-        if (square > 100) System.out.printf("%d squared (%d) is greater than 100%n", number, square);
-        if (square == 100) System.out.printf("%d squared (%d) is equal to 100%n", number, square);
-        if (square < 100) System.out.printf("%d squared (%d) is less than 100%n", number, square);
+        if (square > 100) System.out.printf("%d squared (%d) is greater than 100\n", number, square);
+        if (square == 100) System.out.printf("%d squared (%d) is equal to 100\n", number, square);
+        if (square < 100) System.out.printf("%d squared (%d) is less than 100\n", number, square);
     }
 }
