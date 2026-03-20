b2130e96e4b661f4fe2057a32717908751d82570
devv-leo <lekan.code@gmail.com>
2026-03-20 21:02:16 +0100

    comprehensive test cases covering all payment tiers ((10, 50, 59, 60, 69, 70, 100 deliveries)

--- test/BackToSenderTest.java
+++ test/BackToSenderTest.java
@@ -18,4 +18,39 @@ public class BackToSenderTest {
     public void testZeroDeliveries() {
         assertEquals(5000, BackToSender.calculateWage(0));
     }
+
+    @Test
+    public void testLessThanFiftyPercent() {
+        assertEquals(6600, BackToSender.calculateWage(10));
+    }
+
+    @Test
+    public void testFiftyPercent() {
+        assertEquals(7000, BackToSender.calculateWage(50));
+    }
+
+    @Test
+    public void testFiftyNinePercent() {
+        assertEquals(7180, BackToSender.calculateWage(59));
+    }
+
+    @Test
+    public void testSixtyPercent() {
+        assertEquals(7500, BackToSender.calculateWage(60));
+    }
+
+    @Test
+    public void testSixtyNinePercent() {
+        assertEquals(7725, BackToSender.calculateWage(69));
+    }
+
+    @Test
+    public void testSeventyPercent() {
+        assertEquals(8500, BackToSender.calculateWage(70));
+    }
+
+    @Test
+    public void testHundredPercent() {
+        assertEquals(55000, BackToSender.calculateWage(100));
+    }
 }
