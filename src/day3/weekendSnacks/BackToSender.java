d9eb4374c4745fcd9f4a000701a6f71db291a062
devv-leo <lekan.code@gmail.com>
2026-03-20 21:07:38 +0100

    simplified condition and fixed redundancy

--- src/day3/weekendSnacks/BackToSender.java
+++ src/day3/weekendSnacks/BackToSender.java
@@ -2,7 +2,7 @@ package day3.weekendSnacks;
 
 public class BackToSender {
     public static void main(String[] args) {
-        
+
     }
     
     public static int calculateWage(int successfulDeliveries) {
@@ -11,14 +11,13 @@ public class BackToSender {
         }
         
         int percentage = (successfulDeliveries * 100) / 100;
-        int basePay = 5000;
-        int totalPay = basePay;
+        int totalPay = 5000;
         
         if (percentage < 50) {
             totalPay = totalPay + (successfulDeliveries * 160);
-        } else if (percentage >= 50 && percentage < 60) {
+        } else if (percentage < 60) {
             totalPay = totalPay + (successfulDeliveries * 200);
-        } else if (percentage >= 60 && percentage < 70) {
+        } else if (percentage < 70) {
             totalPay = totalPay + (successfulDeliveries * 250);
         } else {
             totalPay = totalPay + (successfulDeliveries * 500);
