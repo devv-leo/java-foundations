6a53bc291e03383f281390b7bd9f698a91de7228
devv-leo <lekan.code@gmail.com>
2026-03-21 13:38:08 +0100

    feat: implement switchcase for each pizza type and basic calculations

--- src/day3/weekendSnacks/PizzaWahala.java
+++ src/day3/weekendSnacks/PizzaWahala.java
@@ -7,19 +7,53 @@ public class PizzaWahala {
         
         System.out.print("Enter number of guests: ");
         int guests = scanner.nextInt();
-        
-        System.out.print("Enter pizza type (Sapa/Small Money/Big boys/Odogwu): ");
-        scanner.nextLine();
-        String pizzaType = scanner.nextLine();
-        
-        System.out.println("Guests: " + guests);
-        System.out.println("Pizza Type: " + pizzaType);
-        
+
+        String menu = """
+                Enter the Pizza Type:
+                Press 1 -> Sapa Size (4 slices)
+                Press 2 -> Small Money (6 slices)
+                Press 3 -> Big Boys (8 slices)
+                Press 4 -> Odogwu (12 slices)
+                """;
+
+        System.out.print(menu);
+        int pizzaType = scanner.nextInt();
         scanner.close();
-    }
     
-    public static int calculateBoxes(int guests, String pizzaType) {
-        return 0;
+        int pricePerBox = 0;
+        int slices = 0;
+        String size;
+        switch (pizzaType) {
+            case 1 -> {
+                slices = 4;
+                pricePerBox = 2500;
+                size = "Sapa size";
+            }
+            case 2 -> {
+                slices = 6;
+                pricePerBox = 2900;
+                size = "Small money size";
+            }
+            case 3 -> {
+                slices = 8;
+                pricePerBox = 4000;
+                size = "Big boys size";
+            }
+            case 4-> {
+                slices = 12;
+                pricePerBox = 5200;
+                size = "Odogwu size";
+            }
+            default -> {
+                System.out.println("Please pick a valid value");
+                return;
+            }
+        }
+
+        int boxes = (int) Math.ceil((double) guests / slices);
+        int leftoverSlices = slices * boxes - guests;
+        int totalPrice = boxes * pricePerBox;
+    
     }
     
 }
