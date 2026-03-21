da582d0c989fe8696ab88fbadc8365170cd8d3b0
devv-leo <lekan.code@gmail.com>
2026-03-21 09:27:02 +0100

    feat: isEven()

---
+++ src/day3/weekendSnacks/Kata.java
@@ -0,0 +1,13 @@
+package day3.weekendSnacks;
+
+public class Kata {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+    
+    
+}
