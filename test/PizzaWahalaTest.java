faee9c53fef285c76632e5c28d983c50577e817d
devv-leo <lekan.code@gmail.com>
2026-03-22 21:01:54 +0100

    tests: Left over slices & CalculatePrice() test

--- test/PizzaWahalaTest.java
+++ test/PizzaWahalaTest.java
@@ -37,4 +37,22 @@ public class PizzaWahalaTest {
         assertEquals(3, PizzaWahala.calculateBoxes(25, 12));
     }
 
+
+    @Test
+    public void testCalculateLeftoverSlices() {
+        assertEquals(3, PizzaWahala.calculateLeftoverSlices(45, 4, 12));
+        assertEquals(0, PizzaWahala.calculateLeftoverSlices(12, 1, 12));
+        assertEquals(2, PizzaWahala.calculateLeftoverSlices(10, 3, 4));
+        assertEquals(5, PizzaWahala.calculateLeftoverSlices(7, 2, 6));
+        assertEquals(0, PizzaWahala.calculateLeftoverSlices(8, 1, 8));
+    }
+
+    @Test
+    public void testCalculatePrice() {
+        assertEquals(7500, PizzaWahala.calculatePrice(3, 2500));
+        assertEquals(5200, PizzaWahala.calculatePrice(1, 5200));
+        assertEquals(8000, PizzaWahala.calculatePrice(2, 4000));
+        assertEquals(5800, PizzaWahala.calculatePrice(2, 2900));
+        assertEquals(20800, PizzaWahala.calculatePrice(4, 5200));
+    }
 }
