2e2b70e305f4b04fc6515575939df6c6d0c8c8d6
devv-leo <lekan.code@gmail.com>
2026-03-22 22:30:45 +0100

    feat: no. of even & odd numbers in func

--- src/day3/weekendSnacks/arrays/ArrayKata.java
+++ src/day3/weekendSnacks/arrays/ArrayKata.java
@@ -2,37 +2,37 @@ package day3.weekendSnacks.arrays;
 
 public class ArrayKata {
 
-    public int maximumIn(int[] numbers) {
-        int max = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
+    public int maximumIn(int[] arrayOfIntegers) {
+        int max = arrayOfIntegers[0];
+        for (int i = 1; i < arrayOfIntegers.length; i++) {
+            if (arrayOfIntegers[i] > max) {
+                max = arrayOfIntegers[i];
             }
         }
         return max;
     }
 
-    public int minimumIn(int[] numbers) {
-        int min = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] < min) {
-                min = numbers[i];
+    public int minimumIn(int[] arrayOfIntegers) {
+        int min = arrayOfIntegers[0];
+        for (int i = 1; i < arrayOfIntegers.length; i++) {
+            if (arrayOfIntegers[i] < min) {
+                min = arrayOfIntegers[i];
             }
         }
         return min;
     }
 
-    public int sumOf(int[] numbers) {
+    public int sumOf(int[] arrayOfIntegers) {
         int sum = 0;
-        for (int arrayOfInteger : numbers) {
+        for (int arrayOfInteger : arrayOfIntegers) {
             sum += arrayOfInteger;
         }
         return sum;
     }
     
-    public int sumOfEvenNumbersIn(int[] numbers) {
+    public int sumOfEvenNumbersIn(int[] arrayOfIntegers) {
         int sum = 0;
-        for (int arrayOfInteger : numbers) {
+        for (int arrayOfInteger : arrayOfIntegers) {
             if (arrayOfInteger % 2 == 0) {
                 sum += arrayOfInteger;
             }
@@ -40,9 +40,9 @@ public class ArrayKata {
         return sum;
     }
     
-    public int sumOfOddNumbersIn(int[] numbers) {
+    public int sumOfOddNumbersIn(int[] arrayOfIntegers) {
         int sum = 0;
-        for (int arrayOfInteger : numbers) {
+        for (int arrayOfInteger : arrayOfIntegers) {
             if (arrayOfInteger % 2 != 0) {
                 sum += arrayOfInteger;
             }
@@ -56,5 +56,24 @@ public class ArrayKata {
         result[1] = minimumIn(numbers);
         return result;
     }
-    
+
+    public int noOfOddNumbersIn(int[] numbers) {
+        int count = 0;
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int noOfEvenNumbersIn(int[] numbers) {
+        int count = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
