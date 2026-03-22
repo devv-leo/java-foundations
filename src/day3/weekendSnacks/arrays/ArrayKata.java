b0309b8484f7f9e3b8b755f54a034a7ef0ea32de
devv-leo <lekan.code@gmail.com>
2026-03-22 22:26:36 +0100

    feat: maximumAndMinimumOf()

--- src/day3/weekendSnacks/arrays/ArrayKata.java
+++ src/day3/weekendSnacks/arrays/ArrayKata.java
@@ -2,52 +2,59 @@ package day3.weekendSnacks.arrays;
 
 public class ArrayKata {
 
-    public int maximumIn(int[] arrayOfIntegers) {
-        int max = arrayOfIntegers[0];
-        for (int i = 1; i < arrayOfIntegers.length; i++) {
-            if (arrayOfIntegers[i] > max) {
-                max = arrayOfIntegers[i];
+    public int maximumIn(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
             }
         }
         return max;
     }
 
-    public int minimumIn(int[] arrayOfIntegers) {
-        int min = arrayOfIntegers[0];
-        for (int i = 1; i < arrayOfIntegers.length; i++) {
-            if (arrayOfIntegers[i] < min) {
-                min = arrayOfIntegers[i];
+    public int minimumIn(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
             }
         }
         return min;
     }
 
-    public int sumOf(int[] arrayOfIntegers) {
+    public int sumOf(int[] numbers) {
         int sum = 0;
-        for (int i = 0; i < arrayOfIntegers.length; i++) {
-            sum += arrayOfIntegers[i];
+        for (int arrayOfInteger : numbers) {
+            sum += arrayOfInteger;
         }
         return sum;
     }
     
-    public int sumOfEvenNumbersIn(int[] arrayOfIntegers) {
+    public int sumOfEvenNumbersIn(int[] numbers) {
         int sum = 0;
-        for (int i = 0; i < arrayOfIntegers.length; i++) {
-            if (arrayOfIntegers[i] % 2 == 0) {
-                sum += arrayOfIntegers[i];
+        for (int arrayOfInteger : numbers) {
+            if (arrayOfInteger % 2 == 0) {
+                sum += arrayOfInteger;
             }
         }
         return sum;
     }
     
-    public int sumOfOddNumbersIn(int[] arrayOfIntegers) {
+    public int sumOfOddNumbersIn(int[] numbers) {
         int sum = 0;
-        for (int i = 0; i < arrayOfIntegers.length; i++) {
-            if (arrayOfIntegers[i] % 2 != 0) {
-                sum += arrayOfIntegers[i];
+        for (int arrayOfInteger : numbers) {
+            if (arrayOfInteger % 2 != 0) {
+                sum += arrayOfInteger;
             }
         }
         return sum;
+    }
+
+    public int[] maximumAndMinimumOf(int[] numbers) {
+        int[] result = new int[2];
+        result[0] = maximumIn(numbers);
+        result[1] = minimumIn(numbers);
+        return result;
     }
     
 }
