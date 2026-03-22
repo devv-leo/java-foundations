148497fb916c59e6d9b2a2b20e8f8ea4b0e79598
devv-leo <lekan.code@gmail.com>
2026-03-22 22:01:42 +0100

    feat: implement maximumIn & minimumIn

--- src/day3/weekendSnacks/arrays/ArrayKata.java
+++ src/day3/weekendSnacks/arrays/ArrayKata.java
@@ -1,4 +1,24 @@
 package day3.weekendSnacks.arrays;
 
 public class ArrayKata {
+
+    public int maximumIn(int[] arrayOfIntegers) {
+        int max = arrayOfIntegers[0];
+        for (int i = 1; i < arrayOfIntegers.length; i++) {
+            if (arrayOfIntegers[i] > max) {
+                max = arrayOfIntegers[i];
+            }
+        }
+        return max;
+    }
+
+    public int minimumIn(int[] arrayOfIntegers) {
+        int min = arrayOfIntegers[0];
+        for (int i = 1; i < arrayOfIntegers.length; i++) {
+            if (arrayOfIntegers[i] < min) {
+                min = arrayOfIntegers[i];
+            }
+        }
+        return min;
+    }
 }
