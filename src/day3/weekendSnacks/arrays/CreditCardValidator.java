316c1e897a2cd77864a41e00cf258100bd4ef561
devv-leo <lekan.code@gmail.com>
2026-03-22 23:18:27 +0100

    feat: card type detection

--- src/day3/weekendSnacks/arrays/CreditCardValidator.java
+++ src/day3/weekendSnacks/arrays/CreditCardValidator.java
@@ -9,6 +9,27 @@ public class CreditCardValidator {
         System.out.println("Hello, Kindly Enter Card details to verify:");
         String cardNumber = scanner.nextLine();
         
+        String cardType = getCardType(cardNumber);
+        
         scanner.close();
+    }
+    
+    public static String getCardType(String cardNumber) {
+        if (cardNumber.startsWith("37")) {
+            return "American Express";
+        }
+        
+        char firstDigit = cardNumber.charAt(0);
+        
+        switch (firstDigit) {
+            case '4':
+                return "Visa";
+            case '5':
+                return "MasterCard";
+            case '6':
+                return "Discover";
+            default:
+                return "Invalid";
+        }
     }
 }
