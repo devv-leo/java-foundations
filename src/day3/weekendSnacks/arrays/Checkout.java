8577e8b877b86a0638fa0ebeeb78ecfcf93f1ba7
devv-leo <lekan.code@gmail.com>
2026-03-22 23:48:23 +0100

    feat: item input collection & subtotal calc

--- src/day3/weekendSnacks/arrays/Checkout.java
+++ src/day3/weekendSnacks/arrays/Checkout.java
@@ -1,4 +1,5 @@
 package day3.weekendSnacks.arrays;
+
 import java.util.Scanner;
 
 public class Checkout {
@@ -9,6 +10,23 @@ public class Checkout {
         System.out.print("Enter number of items: ");
         int numberOfItems = scanner.nextInt();
         
+        double[] items = new double[numberOfItems];
+        
+        for (int i = 0; i < numberOfItems; i++) {
+            System.out.print("Enter price for item " + (i + 1) + ": ");
+            items[i] = scanner.nextDouble();
+        }
+        
+        double subtotal = calculateSubtotal(items);
+        
         scanner.close();
+    }
+    
+    public static double calculateSubtotal(double[] items) {
+        double sum = 0;
+        for (double price : items) {
+            sum += price;
+        }
+        return sum;
     }
 }
