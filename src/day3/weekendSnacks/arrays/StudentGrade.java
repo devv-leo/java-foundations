2cbe61bc0f1044cbb5d98b57344e85576c50d6ac
devv-leo <lekan.code@gmail.com>
2026-03-23 07:36:44 +0100

    array & input collection

--- src/day3/weekendSnacks/arrays/StudentGrade.java
+++ src/day3/weekendSnacks/arrays/StudentGrade.java
@@ -1,5 +1,21 @@
 package day3.weekendSnacks.arrays;
+import java.util.Scanner;
 
 public class StudentGrade {
-
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = scanner.nextInt();
+        
+        int[] grades = new int[numberOfStudents];
+        
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.print("Enter grade for student " + (i + 1) + ": ");
+            grades[i] = scanner.nextInt();
+        }
+        
+        scanner.close();
+    }
 }
