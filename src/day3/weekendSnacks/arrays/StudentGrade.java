b0cf9abae3591fe498d781d61dae906c0b3766cc
devv-leo <lekan.code@gmail.com>
2026-03-23 08:33:16 +0100

    added correct prompt test and saving messages

--- src/day3/weekendSnacks/arrays/StudentGrade.java
+++ src/day3/weekendSnacks/arrays/StudentGrade.java
@@ -1,4 +1,5 @@
 package day3.weekendSnacks.arrays;
+
 import java.util.Scanner;
 
 public class StudentGrade {
@@ -6,24 +7,29 @@ public class StudentGrade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Enter number of students: ");
+        System.out.println("How many students do you have?");
         int numberOfStudents = scanner.nextInt();
         
-        System.out.print("Enter number of subjects: ");
+        System.out.println("How many subject do they offer?");
         int numberOfSubjects = scanner.nextInt();
+        
+        System.out.println("Saving >>>>>>>>>>>>");
+        System.out.println("Saved successfully");
         
         int[][] grades = new int[numberOfStudents][numberOfSubjects];
         
         for (int i = 0; i < numberOfStudents; i++) {
-            System.out.println("Enter grades for student " + (i + 1) + ":");
+            System.out.println("Entering score for student " + (i + 1));
             for (int j = 0; j < numberOfSubjects; j++) {
-                System.out.print("Subject " + (j + 1) + ": ");
+                System.out.println("Enter score for subject " + (j + 1));
                 int grade = scanner.nextInt();
                 while (grade < 0 || grade > 100) {
-                    System.out.print("Invalid grade! Enter between 0 and 100: ");
+                    System.out.println("Invalid grade! Enter between 0 and 100: ");
                     grade = scanner.nextInt();
                 }
                 grades[i][j] = grade;
+                System.out.println("Saving >>>>>>>>>>>>");
+                System.out.println("Saved successfully");
             }
         }
         
@@ -32,13 +38,11 @@ public class StudentGrade {
         int highest = findHighest(grades);
         int lowest = findLowest(grades);
         
-        System.out.println("Class Average: " + classAverage);
-        System.out.println("Highest Score: " + highest);
-        System.out.println("Lowest Score: " + lowest);
-        System.out.println("Student Averages:");
-        for (int i = 0; i < studentAverages.length; i++) {
-            System.out.println("Student " + (i + 1) + ": " + studentAverages[i]);
-        }
+        System.out.println("*******************");
+        System.out.println("**Class Average: " + classAverage);
+        System.out.println("**Highest Score: " + highest);
+        System.out.println("**Lowest Score: " + lowest);
+        System.out.println("*******************");
         
         scanner.close();
     }
