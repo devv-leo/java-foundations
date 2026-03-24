deba528a433222c2c98f0463a4cb65adf44eb508
devv-leo <lekan.code@gmail.com>
2026-03-24 08:36:37 +0100

    north central zones

--- src/day8/zones.java
+++ src/day8/zones.java
@@ -9,6 +9,14 @@ public class zones {
     public static void main(String[] args) {
         Map<String, String> stateZones = new HashMap<>();
         
+        stateZones.put("Benue", "North Central");
+        stateZones.put("FCT", "North Central");
+        stateZones.put("Kogi", "North Central");
+        stateZones.put("Kwara", "North Central");
+        stateZones.put("Nasarawa", "North Central");
+        stateZones.put("Niger", "North Central");
+        stateZones.put("Plateau", "North Central");
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a Nigerian state: ");
         String state = scanner.nextLine();
