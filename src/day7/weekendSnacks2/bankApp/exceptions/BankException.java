f6bca6af71acaee675eb30fbe94133c7d758a5f1
devv-leo <lekan.code@gmail.com>
2026-03-29 18:08:28 +0100

    add: BankException

---
+++ src/day7/weekendSnacks2/bankApp/exceptions/BankException.java
@@ -0,0 +1,7 @@
+package day7.weekendSnacks2.bankApp.exceptions;
+
+public class BankException extends RuntimeException {
+    public BankException(String message) {
+        super(message);
+    }
+}
